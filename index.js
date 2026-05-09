09d8fedfab5e2e5521b2a1416e06f7a631c70156
nellygath-max <nellygath@gmail.com>
2026-05-09 13:51:09 +0100

    calculator and condition

--- index.js
+++ index.js
@@ -103,3 +103,22 @@ const greet = function(Chinelo)
 
 }; 
 console.log(greet("Chinelo"));
+
+// Build simple calculator
+let num1 = 10;
+let num2 = 5;
+
+let result = num1 + num2;
+
+console.log(result);
+
+
+// Conditions
+let age = 18
+
+if (age >= 18){
+  console.log("You are eligible for voting");
+}
+else{
+  console.log("You are not eligible for voting");
+}
